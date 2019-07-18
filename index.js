a604d02bec1238876aaa507652c6dadaa6fc1642
wjsc <walterscarano@outlook.com>
2019-07-18 16:57:47 -0300

    Working

---
+++ index.js
@@ -0,0 +1,15 @@
+const hold = (operation, ms) => {
+    const symbol = Symbol();
+    let value = symbol;
+    return () => {
+        if(value === symbol) {
+            value = operation();
+            setTimeout(() => {
+                value = symbol;
+            }, ms);
+        }
+        return value;
+    };
+};
+
+module.exports = { hold };
